07979dde23cf60ab323c29d3e58e8d3d7cc95740
Ean <45066060+Nam-SW@users.noreply.github.com>
2024-07-30 18:02:04 +0900

    feat: multi turn agent

--- src/agents/agent.py
+++ src/agents/agent.py
@@ -5,6 +5,7 @@ from langchain_openai import AzureChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_vertexai import ChatVertexAI
 
+from langchain_core.messages import AIMessage, HumanMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.callbacks.manager import get_openai_callback
@@ -43,12 +44,76 @@ def get_chat_chain(prompts: list[tuple[str, str]], **kwargs):
     return chain
 
 
-def get_multi_turn_chat_chain(prompts: list[tuple[str, str]], **kwargs):
-    prompts.insert(1, MessagesPlaceholder(variable_name="history"))
+class MultiTernChain:
+    def __init__(self, system_prompt: str, **kwargs) -> None:
+        self.system_prompt = system_prompt
 
-    prompt_template = PromptTemplate.from_template(prompts)
-    model = get_agent(**kwargs)
-    parser = StrOutputParser()
+        self.model = get_agent(**kwargs)
+        self.parser = StrOutputParser()
+        self.chain = None
+        self.clear_history()
 
-    chain = prompt_template | model | parser
-    return chain
+    @property
+    def chat_history(self):
+        return [
+            f"{'이야기꾼' if isinstance(m, AIMessage) else '유저'}: {m.content}"
+            for m in self.__chat_history
+        ]
+
+    def clear_history(self):
+        self.__chat_history = []
+
+    def set_system_prompt(self, system_prompt_args: dict = {}):
+        self.system_prompt_args = system_prompt_args
+        prompt_template = ChatPromptTemplate.from_messages(
+            [
+                ("system", self.system_prompt.format(**self.system_prompt_args)),
+                MessagesPlaceholder(variable_name="chat_history"),
+                ("user", "{message}"),
+            ]
+        )
+        self.chain = prompt_template | self.model | self.parser
+
+    def get_answer_stream(self, input_text: str):
+        if self.chain is None:
+            raise RuntimeError("chain must be init before answer, through `set_system_prompt()`")
+
+        message = HumanMessage(content=input_text)
+        response = ""
+
+        for chunk in self.chain.stream(
+            {
+                "message": message,
+                "chat_history": self.__chat_history,
+            },
+            # config={"metadata": {"conversation_id": conversation_id}},
+        ):
+            yield chunk
+            response += chunk
+        else:
+            response = self.chain.invoke(
+                {
+                    "message": message,
+                    "chat_history": self.__chat_history,
+                }
+            )
+
+        self.__chat_history.extend(
+            [
+                message,
+                AIMessage(content=response),
+            ]
+        )
+
+    def get_answer(self, input_text: str):
+        if self.chain is None:
+            raise RuntimeError("chain must be init before answer, through `set_system_prompt()`")
+
+        response = self.chain.invoke({"message": input_text, "chat_history": self.__chat_history})
+        self.__chat_history.extend(
+            [
+                HumanMessage(content=input_text),
+                AIMessage(content=response),
+            ]
+        )
+        return response
