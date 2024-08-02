420fbe693e235c9f2fe5585490e72a23cf067f53
Ean <45066060+Nam-SW@users.noreply.github.com>
2024-08-03 03:17:55 +0900

    feat: utility funk

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,9 +1,32 @@
 import json
+import re
+from typing import Any
 
 
 def convert_json(text: str) -> dict:
+    group = re.search(r"```(?:json\s*)?([\s\S]*?)```", text, re.S)
+    if group is not None:
+        text = group.group(1)
+
     try:
+        text = re.search(r"(\{.+\})?", text, re.S).group()
+        text = re.sub(r"\n+", " ", text)
         result = eval(text)
     except:
-        result = json.loads(text)
+        try:
+            result = json.loads(text)
+        except:
+            raise SyntaxError(f"can't convert json from context: \n{text}")
     return result
+
+
+def try_n(func: callable, args: list = [], kwargs: dict = {}, n: int = 10) -> Any:
+    i = 0
+    while True:
+        i += 1
+        try:
+            result = func(*args, **kwargs)
+            return result
+        except Exception as e:
+            if i >= n:
+                raise e
